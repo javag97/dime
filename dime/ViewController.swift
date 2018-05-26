2f698005eec92798c20c7b893c57a0da51bd9121
Javier Garcia <javier.garcia549@gmail.com>
2018-05-26 14:25:54 -0700

    Initial Commit

---
+++ dime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dime
+//
+//  Created by Javier Garcia on 5/26/18.
+//  Copyright © 2018 Javier Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
